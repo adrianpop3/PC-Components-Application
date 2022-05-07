8f2885368eb52ae6d41ac544775afbaba8761c08
Tudor Pop <tudor.pop@tum.de>
2022-05-07 20:17:35 +0300

    PCA-38

--- src/main/java/org/loose/fis/sre/controllers/RegistrationController.java
+++ src/main/java/org/loose/fis/sre/controllers/RegistrationController.java
@@ -1,36 +1,58 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
-import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 import org.loose.fis.sre.services.UserService;
+import org.loose.fis.sre.model.User;
+import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
 
-public class RegistrationController {
+import java.util.Random;
 
+public class RegistrationPageController {
     @FXML
-    private Text registrationMessage;
+    private TextField username;
     @FXML
-    private PasswordField passwordField;
+    private TextField email;
     @FXML
-    private TextField usernameField;
+    private TextField password;
     @FXML
-    private ChoiceBox role;
+    private TextField passwordConfirmation;
+    @FXML
+    private Button registerB;
+    @FXML
+    private CheckBox terms, news;
+    @FXML
+    private CheckBox hide;
+    @FXML
+    private PasswordField hidpas1;
+    @FXML
+    private PasswordField hidpas2;
+    @FXML
+    private Hyperlink gotoLOGIN;
+    @FXML
+    private ComboBox comboBox;
+    private Parent root;
+    private Stage stage;
+    private Alert alert, alert1, alertUsername, alertPasswordIncorect;
+    private static int id = 0;
+    private Button myButton;
 
-    @FXML
-    public void initialize() {
-        role.getItems().addAll("Customer", "Seller");
+
+    public void SwitchPage(){
+        System.out.println("Test");
     }
 
-    @FXML
-    public void handleRegisterAction() {
-        try {
-            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
-            registrationMessage.setText("Account created successfully!");
-        } catch (UsernameAlreadyExistsException e) {
-            registrationMessage.setText(e.getMessage());
-        }
+    public void ShowHide(){
+        System.out.println("Test");
+    }
+
+    public void registerB(){
+        System.out.println("Test");
     }
 }
