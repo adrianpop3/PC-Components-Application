d0e508f96c6d34db47363948a05bb4593e3d5db6
adrianpop3 <adrian.pop2@student.upt.ro>
2022-05-08 18:56:55 +0300

    PCA-42

--- src/main/java/org/loose/fis/sre/controllers/HomePageController.java
+++ src/main/java/org/loose/fis/sre/controllers/HomePageController.java
@@ -1,7 +1,55 @@
 package org.loose.fis.sre.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 public class HomePageController {
-    //Not implemented yet
+
+    @FXML
+    private Text welcomeTxt;
+
+    @FXML
+    private Button processors, graphic, ram, ssdhdd, add, delete, edit, placeOrder, approve, status, sellerHistory, customerHistory, logout;
+
+    public void handleLinkToCategoriesAction(ActionEvent actionEvent) {
+        //to be done
+    }
+
+    public void handleLogOutAction(ActionEvent actionEvent) {
+        //to be done
+    }
+
+    public void handleAddProductAction(ActionEvent actionEvent) {
+        //to be done
+    }
+
+    public void handleDeleteProductAction(ActionEvent actionEvent) {
+        //to be done
+    }
+
+    public void handleEditProductAction(ActionEvent actionEvent) {
+        //to be done
+    }
+
+    public void handlePlaceOrderAction(ActionEvent actionEvent) {
+        //to be done
+    }
+
+    public void handleApproveOrderAction(ActionEvent actionEvent) {
+        //to be done
+    }
+
+    public void handleOnViewStatusAction(ActionEvent actionEvent) {
+        //to be done
+    }
+
+    public void handleOnSellerHistoryAction(ActionEvent actionEvent) {
+        //to be done
+    }
+
+    public void handleOnCustomerHistoryAction(ActionEvent actionEvent) {
+        //to be done
+    }
 }
