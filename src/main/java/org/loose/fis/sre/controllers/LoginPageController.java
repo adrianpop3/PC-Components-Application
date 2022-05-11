8f50a2cb909f4224f57c714c6d6c8e7e074dbf8f
Tudor Pop <tudor.pop@tum.de>
2022-05-11 09:12:50 +0300

    PCA-2 Implemented LoginPageController with attributes and empty methods.

---
+++ src/main/java/org/loose/fis/sre/controllers/LoginPageController.java
@@ -0,0 +1,28 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class LoginPageController {
+
+    @FXML
+    private TextField password, username;
+    @FXML
+    private CheckBox passwordVisibility;
+    @FXML
+    private PasswordField hiddenPassword;
+    @FXML
+    private Button loginButton;
+
+    public void handleLoginAction(){
+        // To be implemented
+    }
+
+    public void handlePasswordVisibilityAction() {
+        // To be implemented
+    }
+
+    public void handleLinkToRegistrationAction() {
+        // To be implemented
+    }
+}
