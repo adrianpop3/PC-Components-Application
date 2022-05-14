3a7d9849b02299ea5e6a61c372d0b7624e725dc1
adrianpop3 <adrian.pop2@student.upt.ro>
2022-05-14 18:45:18 +0300

    PCA-42 fxml for processors page added & controller created

--- src/main/java/org/loose/fis/sre/products/processors/Processors.java
+++ src/main/java/org/loose/fis/sre/products/processors/Processors.java
@@ -1,4 +1,14 @@
 package org.loose.fis.sre.products.processors;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class Processors {
+    public Button back;
+    public AnchorPane anchorPaneRight;
+
+    public void BackToHomePage(ActionEvent actionEvent) {
+        //to be done
+    }
 }
