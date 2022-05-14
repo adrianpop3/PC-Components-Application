d2c1b5f5f37ca77fc425a8ee8819fccf49ba98e4
adrianpop3 <adrian.pop2@student.upt.ro>
2022-05-14 20:02:07 +0300

    PCA-42 created controllers for all remaining the products

--- src/main/java/org/loose/fis/sre/products/ssdhdd/SSD_HDD.java
+++ src/main/java/org/loose/fis/sre/products/ssdhdd/SSD_HDD.java
@@ -1,4 +1,14 @@
 package org.loose.fis.sre.products.ssdhdd;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class SSD_HDD {
+    public Button back;
+    public AnchorPane anchorPaneRight;
+
+    public void BackToHomePage(ActionEvent actionEvent) {
+        //to be done
+    }
 }
