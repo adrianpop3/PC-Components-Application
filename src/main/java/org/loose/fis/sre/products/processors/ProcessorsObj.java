db3dd839369636ef4ab1dfcb05eacdb6ed61a622
adrianpop3 <adrian.pop2@student.upt.ro>
2022-05-15 02:33:33 +0300

    PCA-7 class for category Processors added

---
+++ src/main/java/org/loose/fis/sre/products/processors/ProcessorsObj.java
@@ -0,0 +1,64 @@
+package org.loose.fis.sre.products.processors;
+
+public class ProcessorsObj {
+
+    private String numeProdus;
+    private String pret;
+    private String descriere;
+    private String garantie;
+    private int id;
+
+    //Constructors:
+
+    public ProcessorsObj() {}
+
+    public ProcessorsObj (String numeProdus, String pret, String descriere, String garantie, int id) {
+        this.numeProdus = numeProdus;
+        this.pret = pret;
+        this.descriere = descriere;
+        this.garantie = garantie;
+        this.id = id;
+    }
+
+    //Getters:
+
+    public String getNumeProdus() {
+        return numeProdus;
+    }
+
+    public String getPret() {
+        return pret;
+    }
+
+    public String getDescriere() {
+        return descriere;
+    }
+
+    public String getGarantie() {
+        return garantie;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    //Setters:
+
+    public void setPret(String pret) {
+        this.pret = pret;
+    }
+
+    public void setDescriere(String descriere) {
+        this.descriere = descriere;
+    }
+
+    public void setGarantie(String garantie) {
+        this.garantie = garantie;
+    }
+
+    @Override
+    public String toString() {
+        return numeProdus + " " + pret + " " + descriere + " " + garantie;
+    }
+
+}
