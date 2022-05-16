e1804e56a840227c565b3627985b2bc910aebc8e
adrianpop3 <adrian.pop2@student.upt.ro>
2022-05-16 13:06:02 +0300

    PCA-7 PopUpEdit page configured and controller initialized

---
+++ src/main/java/org/loose/fis/sre/controllers/PopUpController.java
@@ -0,0 +1,26 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Pane;
+
+public class PopUpController {
+
+    @FXML
+    public ComboBox comboBox1;
+    public Pane panelProcessors1;
+    public Pane panelGraphic1;
+    public Pane panelRAM1;
+    public Pane panelSSD_HDD1;
+    public Button ClosePopUp1;
+
+    public void initializeEditPopUp (ActionEvent actionEvent) {
+        //to be done
+    }
+
+    public void CloseEditPopUp (ActionEvent actionEvent) {
+        //to be done
+    }
+}
