0dfc1fa1915fc493757fe935540c28324f773aa1
Pop Tudor <>
2022-05-22 17:00:27 +0300

    PCA-6 Finished the implementation of displayProduct functionality in SSD_HDD.

--- src/main/java/org/loose/fis/sre/services/SSD_HDDService.java
+++ src/main/java/org/loose/fis/sre/services/SSD_HDDService.java
@@ -7,25 +7,30 @@ import org.loose.fis.sre.controllers.PopUpController;
 import org.loose.fis.sre.products.ssdhdd.SSD_HDD_Obj;
 
 import java.util.List;
+import java.util.Objects;
 
 import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
 
 public class SSD_HDDService {
     private static ObjectRepository<SSD_HDD_Obj> SSDHDDRepository;
     private static Nitrite database;
-    private static int i=0;
+    private static int i = 0;
 
-    public static void initDataBaseSSDHDD(){
+    public static void initDataBaseSSDHDD() {
         FileSystemService.initDirectory();
         database = Nitrite.builder()
                 .filePath(getPathToFile("SSDHDD.db").toFile())
                 .openOrCreate("test", "test");
-        SSDHDDRepository= database.getRepository(SSD_HDD_Obj.class);
+        SSDHDDRepository = database.getRepository(SSD_HDD_Obj.class);
     }
 
-    public static void closeDataBase(){database.close();}
+    public static void closeDataBase() {
+        database.close();
+    }
 
-    public static List<SSD_HDD_Obj> getAllProduct(){return SSDHDDRepository.find().toList();}
+    public static List<SSD_HDD_Obj> getAllProduct() {
+        return SSDHDDRepository.find().toList();
+    }
 
     /*public static void EditProduct(String numeProdus,String Pret,String Specific,String Garantie,String Descriere) {
         for(SSD_HDD_Obj sourcesBase : SSDHDDRepository.find())
@@ -40,30 +45,26 @@ public class SSD_HDDService {
         }
     }*/
 
-    public static void setForDelete(){
-        for(SSD_HDD_Obj sourcesBase : SSDHDDRepository.find())
-        {
-            if(UserService.returnId(HomePageController.getUsernameHome()) == sourcesBase.getId()){
+    public static void setForDelete() {
+        for (SSD_HDD_Obj sourcesBase : SSDHDDRepository.find()) {
+            if (UserService.returnId(HomePageController.getUsernameHome()) == sourcesBase.getId()) {
                 i++;
-                PopUpController.getDataBase(sourcesBase.getNumeProdus(),sourcesBase.getPret(),sourcesBase.getDescriere(),sourcesBase.getSpecific(), sourcesBase.getGarantie(),"b"+i);
+                PopUpController.getDataBase(sourcesBase.getNumeProdus(), sourcesBase.getPret(), sourcesBase.getDescriere(), sourcesBase.getSpecific(), sourcesBase.getGarantie(), "b" + i);
             }
         }
     }
 
-    public static void addSSDHDD(String numeProdus, String pret, String specific, String descriere, String garantie,int id) {
+    public static void addSSDHDD(String numeProdus, String pret, String specific, String descriere, String garantie, int id) {
         SSDHDDRepository.insert(new SSD_HDD_Obj(numeProdus, specific, pret, descriere, garantie, id));
     }
 
-    public static int returnId(String numeProdus){
-        for(SSD_HDD_Obj sourcesBase : SSDHDDRepository.find())
-        {
-            if (numeProdus.equals(sourcesBase.getNumeProdus())) {
-                return sourcesBase.getId();
+    public static int returnProductId(String productName) {
+        for (SSD_HDD_Obj ssd_hdd_obj : SSDHDDRepository.find()) {
+            if (Objects.equals(productName, ssd_hdd_obj.getNumeProdus())) {
+                return ssd_hdd_obj.getId();
             }
         }
-        return  -1;
-
+        return -1;
     }
-
 
 }
