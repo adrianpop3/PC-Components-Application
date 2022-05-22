ba5689855019e621adc7d9d7c87cd163fb450500
Pop Tudor <>
2022-05-22 20:44:58 +0300

    PCA-8 Created the structure for the Order Status functionality.

---
+++ src/main/java/org/loose/fis/sre/controllers/OrderStatusController.java
@@ -0,0 +1,17 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class OrderStatusController {
+
+    @FXML
+    private Button closeButton;
+    @FXML
+    private AnchorPane anchorPane;
+
+    public void handleCloseAction() {
+        // to be implemented
+    }
+}
