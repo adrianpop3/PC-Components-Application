b6d66c3322d9ae43d7913faca21c4f97d081ce66
Pop Tudor <>
2022-05-22 13:09:55 +0300

    PCA-6 Added helper methods in OrderService and deleted useless checkBox from PopUpMyCart.fxml

--- src/main/java/org/loose/fis/sre/services/OrderService.java
+++ src/main/java/org/loose/fis/sre/services/OrderService.java
@@ -1,4 +1,27 @@
 package org.loose.fis.sre.services;
 
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.loose.fis.sre.model.Order;
+
+import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
+
 public class OrderService {
+
+    private static ObjectRepository<Order> orderObjectRepository;
+    private static Nitrite database;
+
+    public static void initDataBaseOrder() {
+        FileSystemService.initDirectory();
+        database = Nitrite.builder()
+                .filePath(getPathToFile("Order.db").toFile())
+                .openOrCreate("test", "test");
+        orderObjectRepository = database.getRepository(Order.class);
+    }
+
+    public static void addOrder(String sellerName, String customerName, String productName, int quantity, String status, int customerId, int orderId) {
+        orderObjectRepository.insert(new Order(sellerName, customerName, productName, quantity, status, customerId, orderId));
+    }
+
+
 }
