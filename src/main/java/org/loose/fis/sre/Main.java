7e5b503f8cd3783ed411566368eca0f70747d963
Pop Tudor <>
2022-05-22 13:20:37 +0300

    PCA-6 Initiated database for orders.

--- src/main/java/org/loose/fis/sre/Main.java
+++ src/main/java/org/loose/fis/sre/Main.java
@@ -5,10 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.loose.fis.sre.services.GraphicService;
-import org.loose.fis.sre.services.ProcessorService;
-import org.loose.fis.sre.services.TemporaryOrderService;
-import org.loose.fis.sre.services.UserService;
+import org.loose.fis.sre.services.*;
 
 public class Main extends Application {
 
@@ -18,6 +15,7 @@ public class Main extends Application {
         ProcessorService.initDataBaseProcessors();
         GraphicService.initDataBaseGraphic();
         TemporaryOrderService.initDataBaseTemporaryOrder();
+        OrderService.initDataBaseOrder();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/pages/starting_page.fxml"));
         primaryStage.setTitle("PCA - START");
         primaryStage.setScene(new Scene(root));
