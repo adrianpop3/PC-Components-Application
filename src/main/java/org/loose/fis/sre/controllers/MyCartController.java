d52b06d2693d2e5da9b3ea737fee72d496d88bb2
Pop Tudor <>
2022-05-22 11:47:31 +0300

    PCA-6 Created the structure (fxml + Controller) for MyCart functionality.

---
+++ src/main/java/org/loose/fis/sre/controllers/MyCartController.java
@@ -0,0 +1,32 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class MyCartController {
+
+    @FXML
+    private AnchorPane anchorPane;
+    private Stage stage;
+
+    @FXML
+    private Button placeOrder, cancelOrder;
+    @FXML
+    private CheckBox expressDelivery;
+
+    public void handlePlaceOrderAction() {
+        // to be implemented
+    }
+
+    public void handleCancelOrder() {
+        // to be implemented
+    }
+
+    public void handleExpressDeliveryAction() {
+        // to be implemented
+    }
+
+}
