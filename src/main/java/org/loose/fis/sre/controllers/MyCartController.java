e33a25504eeb31e7bdb7d9581a4df449f1c84745
Pop Tudor <>
2022-05-22 12:18:39 +0300

    PCA-6 Implemented the displayProduct functionality - NOT TESTED YET

--- src/main/java/org/loose/fis/sre/controllers/MyCartController.java
+++ src/main/java/org/loose/fis/sre/controllers/MyCartController.java
@@ -1,21 +1,67 @@
 package org.loose.fis.sre.controllers;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyCartController {
 
     @FXML
     private AnchorPane anchorPane;
+    private static VBox vBox = new VBox();
+    private static Pane[] pane = new Pane[10];
+    private static List<Text> name = new ArrayList<>(10);
+    private static List<Text> quantity = new ArrayList<>(10);
+
     private Stage stage;
 
     @FXML
     private Button placeOrder, cancelOrder;
     @FXML
     private CheckBox expressDelivery;
+
+    @FXML
+    public void initialize() {
+        vBox = new VBox();
+        vBox.setPadding(new Insets(10, 10, 10, 10));
+        vBox.setSpacing(50);
+
+        anchorPane.getChildren().add(vBox);
+    }
+
+
+    public static void displayProducts(String productName, String productQuantity) {
+
+        for (int i = 0; i < 10; i++) {
+
+            // Name
+            name.add(i, new Text(productName));
+            name.get(i).setLayoutX(0);
+            name.get(i).setLayoutY(3);
+
+            // Quantity
+            quantity.add(i, new Text(productQuantity));
+            quantity.get(i).setLayoutY(3);
+            quantity.get(i).setLayoutX(100);
+
+            pane[i] = new Pane();
+            pane[i].setLayoutX(300);
+            pane[i].setLayoutY(50);
+            pane[i].getChildren().addAll(name.get(i), quantity.get(i));
+
+        }
+
+        vBox.getChildren().add(pane[PopUpController.getMaxNrProducts()]);
+    }
 
     public void handlePlaceOrderAction() {
         // to be implemented
